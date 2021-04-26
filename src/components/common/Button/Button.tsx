2e04e4e404525ca6f93c571b74532b16dbaf4f58
tsanev96 <tsanev96@gmail.com>
2021-04-26 20:11:12 +0300

    added pathname prop for btn

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,19 +1,31 @@
 import React, { FC } from "react";
 import "./_button.scss";
+import { Link } from "react-router-dom";
 
 interface PropsButton {
   text: string;
+  pathname?: string;
   theme?: "dark" | "light";
 }
 
-export const Button: FC<PropsButton> = ({ text, theme = "light" }) => {
+export const Button: FC<PropsButton> = ({
+  text,
+  theme = "light",
+  pathname,
+}) => {
+  const classes = `button ${
+    theme === "light" ? "button__light" : "button__dark"
+  }`;
+
   return (
-    <button
-      className={`button ${
-        theme === "light" ? "button__light" : "button__dark"
-      }`}
-    >
-      {text}
-    </button>
+    <>
+      {pathname ? (
+        <Link className={classes} to={pathname}>
+          {text}
+        </Link>
+      ) : (
+        <button className={classes}>{text}</button>
+      )}
+    </>
   );
 };
